90a6fcad15322038eec43aaf2077db967543af69
linorcohen <linorlaurie@gmail.com>
2021-08-02 20:49:04 +0300

    ex5 commit -  initial docstring and comments

--- Exercises/Ex5/wordsearch.py
+++ Exercises/Ex5/wordsearch.py
@@ -10,7 +10,16 @@ ILLEGAL_DIRECTIONS: str = 'Directions contain illegal characters. '
 DIRECTIONS: Set[str] = {'u', 'd', 'r', 'l', 'w', 'x', 'y', 'z'}
 
 
-def check_input_args(args: List[str]) -> Optional[str]:
+def check_input_args(args):
+    """
+    This function checks if the input arguments are valid to use.
+    if not valid - returns an informative massage accordingly.
+    if valid - returns None.
+    :param args: list of arguments
+    :type args: list[str]
+    :return: massage, else None.
+    :rtype: str or None
+    """
     if len(args) != 4:
         return INVALID_NUM_MASSAGE
     if not os.path.isfile(args[0]) or not os.path.isfile(args[1]):
@@ -19,122 +28,218 @@ def check_input_args(args: List[str]) -> Optional[str]:
         return ILLEGAL_DIRECTIONS
 
 
-def contain_only_legal_directions(directions: str) -> bool:
+def contain_only_legal_directions(directions):
+    """
+    This function checks if the directions contains only legal directions.
+    :param directions: matrix search directions
+    :type directions: str
+    :return: False if found illegal directions, else True.
+    :rtype: bool
+    """
     for i in directions:
         if i not in DIRECTIONS:
             return False
     return True
 
 
-def read_wordlist(filename: str) -> List[str]:
-    file_content = file_content_list(filename)
-    words_lst = [line.strip() for line in file_content]
-    return words_lst
-
-
-def file_content_list(filename: str) -> List[str]:
+def read_wordlist(filename):
+    """
+    This function open and reads the words file and returns a list of words.
+    :param filename: words file path
+    :type filename: str
+    :return: list of words
+    :rtype: list[str]
+    """
     with open(filename, 'r') as words_file:
-        file_content = words_file.readlines()
-        return file_content
+        return [line.strip() for line in words_file.readlines()]
 
 
-def read_matrix(filename: str) -> List[List[str]]:
-    file_content = file_content_list(filename)
-    matrix = [(line.strip()).split(',') for line in file_content]
-    return matrix
+def read_matrix(filename: str):
+    """
+    This function open and reads the matrix file and returns a list of lists
+    of letters.
+    :param filename: matrix file path
+    :type filename: str
+    :return: 2D list of the matrix letters
+    :rtype: list[list[str]]
+    """
+    with open(filename, 'r') as words_file:
+        return [line.strip().split(',') for line in words_file.readlines()]
 
 
-def find_words(word_list: List[str], matrix: List[List[str]], directions: str) -> List[Tuple[str, int]]:
+def find_words(word_list, matrix, directions):
+    """
+    This function search and counts the number of words is the matrix according
+    to the directions given and returns them in a list of couples (word, count)
+    where word is a word that appears in the matrix, and count is the times the
+    same word was found.
+    :param word_list: list of words
+    :type word_list: list[str]
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :param directions: directions to search through
+    :type directions: str
+    :return: list of couples (word, count)
+    :rtype: list[tuple[str,int]]
+    """
     if len(word_list) == 0 or len(matrix) == 0:
-        return []
-    words_count_dict: Dict[str, int] = {}
+        return []  # in case of empty matrix or word list
+    words_count_dict = {}
     for direction in set(directions):
-        matrix_copy = copy.deepcopy(matrix)
-        lst = return_list_according_to_directions(direction, matrix_copy)
-        count_words(lst, word_list, words_count_dict)
-    # convert dictionary to tuple
+        matrix_copy = copy.deepcopy(matrix)  # so we won't change the original matrix
+        directions_lst = return_list_according_to_directions(direction, matrix_copy)
+        count_words(directions_lst, word_list, words_count_dict)
+    # convert dictionary to tuple:
     return [(k, v) for k, v in words_count_dict.items()]
 
 
-def return_list_according_to_directions(direction, matrix_copy):
+def return_list_according_to_directions(direction, matrix):
+    """
+    This function checks the direction of the search and returns a list of
+    organized matrix lines joined according to that direction.
+    :param direction: direction to search through
+    :type direction: str
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :return: list of organized matrix lines joined
+    :rtype: list[str]
+    """
     if direction == 'u':  # ↑
-        return up(matrix_copy)
-    elif direction == 'd':  # ↓
-        return down(matrix_copy)
-    elif direction == 'r':  # →
-        return right(matrix_copy)
-    elif direction == 'l':  # ←
-        return left(matrix_copy)
-    if direction == 'w':  # ↗
-        return diagonal_rise_right(matrix_copy)
-    if direction == 'x':  # ↖
-        return diagonal_rise_left(matrix_copy)
-    if direction == 'y':  # ↘
-        return diagonal_descends_right(matrix_copy)
-    if direction == 'z':  # ↙
-        return diagonal_descends_left(matrix_copy)
+        return up(matrix)
+    if direction == 'd':  # ↓
+        return down(matrix)
+    if direction == 'r':  # →
+        return right(matrix)
+    if direction == 'l':  # ←
+        return left(matrix)
+    return diagonal_search(matrix, direction)
 
 
-#  fix variables name:
-def diagonal_rise_left(matrix: List[List[str]]) -> List[str]:  # ↖
-    rows = len(matrix)
+def diagonal_search(matrix, direction):
+    """
+    This function checks the direction of diagonal search and returns a list of
+    organized matrix lines joined according to that diagonal direction.
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :param direction: diagonal direction to search through
+    :type direction: str
+    :return: list of organized matrix lines joined
+    :rtype: list[str]
+    """
     columns = len(matrix[0])
+    if direction == 'w':  # ↗
+        columns_direction = list(range(columns))  # [0, 1, 2, 3, ...]
+        return diagonal_raise(columns, matrix, columns_direction)
+    if direction == 'z':  # ↙
+        columns_direction = list(range(columns))
+        return diagonal_descends(columns, matrix, columns_direction)
+    if direction == 'x':   # ↖
+        columns_direction = list(reversed(range(columns)))  # [..., 3, 2, 1, 0]
+        return diagonal_raise(columns, matrix, columns_direction)
+    if direction == 'y':  # ↘
+        columns_direction = list(reversed(range(columns)))
+        return diagonal_descends(columns, matrix, columns_direction)
+
+
+def diagonal_raise(columns, matrix, columns_direction):
+    """
+    This function returns a list of organized matrix lines joined in the
+    direction of diagonal raise left or right according to the columns_direction.
+    :param columns: number of columns in matrix
+    :type columns: int
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :param columns_direction: list of columns direction
+    :type columns_direction: list[int]
+    :return: list of organized matrix lines joined
+    :rtype: list[str]
+    """
+    rows = len(matrix)
     diagonal_matrix = [[] for i in range(rows + columns - 1)]
-    n = 0
-    for i in reversed(range(columns)):
-        m = n
+    diagonal_matrix_index = 0
+    for i in columns_direction:
+        m = diagonal_matrix_index
         for j in range(rows):
             diagonal_matrix[m].append(matrix[j][i])
             m += 1
-        n += 1
+        diagonal_matrix_index += 1
     return [''.join(lst) for lst in diagonal_matrix]
 
 
-# duplicate code
-def diagonal_descends_right(matrix: List[List[str]]) -> List[str]:  # ↘
-    return [string[::-1] for string in diagonal_rise_left(matrix)]
+def diagonal_descends(columns, matrix, columns_direction):
+    """
+    This function returns a list of organized matrix lines joined in the
+    direction of diagonal descend left or right according to the columns_direction.
+    :param columns: number of columns in matrix
+    :type columns: int
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :param columns_direction: list of columns direction
+    :type columns_direction: list[int]
+    :return: list of organized matrix lines joined
+    :rtype: list[str]
+    """
+    return [string[::-1] for string in diagonal_raise(columns, matrix, columns_direction)]
 
 
-def diagonal_rise_right(matrix: List[List[str]]) -> List[str]:  # ↗
-    rows = len(matrix)
-    columns = len(matrix[0])
-    diagonal_left_matrix = [[] for i in range(rows + columns - 1)]
-    n = 0
-    for i in range(columns):
-        m = n
-        for j in range(rows):
-            diagonal_left_matrix[m].append(matrix[j][i])
-            m += 1
-        n += 1
-    return [''.join(lst) for lst in diagonal_left_matrix]
+def up(matrix):  # ↑
+    """
+    This function returns a list of organized matrix lines joined in the up direction.
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :return: a list of organized matrix lines joined
+    :rtype: list[str]
+    """
+    return [''.join([row[i] for row in matrix[::-1]]) for i in range(len(matrix[0]))]
 
 
-def diagonal_descends_left(matrix: List[List[str]]) -> List[str]:  # ↙
-    return [string[::-1] for string in diagonal_rise_right(matrix)]
+def down(matrix):  # ↓
+    """
+    This function returns a list of organized matrix lines joined in the down direction.
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :return: a list of organized matrix lines joined
+    :rtype: list[str]
+    """
+    return [''.join([row[i] for row in matrix]) for i in range(len(matrix[0]))]
 
 
-def up(matrix: List[List[str]]) -> List[str]:  # ↑
-    up_order_matrix = [[row[i] for row in matrix[::-1]] for i in range(len(matrix[0]))]
-    return [''.join(lst) for lst in up_order_matrix]
-
-
-def down(matrix: List[List[str]]) -> List[str]:  # ↓
-    down_order_matrix = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
-    return [''.join(lst) for lst in down_order_matrix]
-
-
-def right(matrix: List[List[str]]) -> List[str]:  # →
+def right(matrix):  # →
+    """
+    This function returns a list of organized matrix lines joined in the right direction.
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :return: a list of organized matrix lines joined
+    :rtype: list[str]
+    """
     return [''.join(lst) for lst in matrix]
 
 
-def left(matrix: List[List[str]]) -> List[str]:  # ←
+def left(matrix):  # ←
+    """
+    This function returns a list of organized matrix lines joined in the left direction.
+    :param matrix: 2D list of the matrix letters
+    :type matrix: list[list[str]]
+    :return: a list of organized matrix lines joined
+    :rtype: list[str]
+    """
     return [''.join(lst[::-1]) for lst in matrix]
 
 
-def count_words(strings_lst: List[str], words_lst: List[str], words_count_dict: Dict[str, int]) -> None:
+def count_words(lines_lst, words_lst, words_count_dict):
+    """
+    This function get a list of lines and counts the number of times a word
+    from the words list is shown, and then updates the dictionary.
+    :param lines_lst: list of matrix lines joined
+    :type lines_lst: list[str]
+    :param words_lst: list of words
+    :type words_lst: list[str]
+    :param words_count_dict: dictionary that stores the words and their counts
+    :type words_count_dict: dict[str,int]
+    """
     for word in words_lst:
-        for string in strings_lst:
-            count = len([i for i in range(len(string)) if string.startswith(word, i)])
+        for line in lines_lst:
+            count = len([i for i in range(len(line)) if line.startswith(word, i)])
             if count > 0:
                 if word in words_count_dict:
                     words_count_dict[word] += count
@@ -142,13 +247,27 @@ def count_words(strings_lst: List[str], words_lst: List[str], words_count_dict: 
                     words_count_dict[word] = count
 
 
-def write_output(results: List[Tuple[str, int]], filename: str) -> None:
+def write_output(results, filename):
+    """
+    This function write to file the results of the words search in the matrix.
+    :param results: list of couples of (word, count)
+    :type results: list[tuple[str,int]]
+    :param filename: file path to write the results to
+    :type filename: str
+    """
     with open(filename, 'w') as output_file:
         for couple in results:
             output_file.write(couple[0]+','+str(couple[1])+'\n')
 
 
-def main(args) -> None:
+def main(args):
+    """
+    This function gets list of args [word_file, matrix_file, output_file, directions],
+    search words in matrix according to the directions and write
+    the results to a file.
+    :param args: list of args
+    :type args: list[str]
+    """
     if check_input_args(args) is not None:
         print(check_input_args(args))
         return
